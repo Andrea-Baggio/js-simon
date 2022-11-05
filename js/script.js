d771b599a34571d3e058155c7501260fe9255293
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-05 22:24:01 +0100

    numeri random fatti male

--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
-/*
-Descrizione:
-Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma.
-*/
-
 const eleStartButton = document.querySelector('#first-block');
 const eleRules = document.querySelector('.rules');
 const eleBar = document.querySelector('.bar');
 const eleSecondBar = document.querySelector('.second-bar');
 const eleGame = document.querySelector('.game');
-
-// const arrNumbers = ['1'];
-// const userNumbers = prompt('Inserisci i numeri che ricordi!');
 
 function startButton () {
     eleStartButton.classList.add('hidden');
@@ -37,27 +23,25 @@ function startButton () {
         eleGame.classList.add('hidden'); 
         eleSecondBar.classList.add('hidden'); 
     }, 37000);
-    
+
     const arrNumbers = [];
 
-    const randomNumbers = Math.floor(Math.random() * 100 + 1);
+    const randomNumbers1 = Math.floor(Math.random() * 100);
+    const randomNumbers2 = Math.floor(Math.random() * 100);
+    const randomNumbers3 = Math.floor(Math.random() * 100);
+    const randomNumbers4 = Math.floor(Math.random() * 100);
+    const randomNumbers5 = Math.floor(Math.random() * 100);
+
     for (let i = 0; i < 5; i++) {  
-        arrNumbers.push(randomNumbers);       
-        console.log(arrNumbers);            
+        arrNumbers.push(randomNumbers1, randomNumbers2, randomNumbers3, randomNumbers4, randomNumbers5);       
+        console.log(arrNumbers[i]);        
        
-    eleGame.innerHTML += arrNumbers[i] + ' ';
-}
+        eleGame.innerHTML += arrNumbers[i] + ' ';
+    }
 }
 
 eleStartButton.addEventListener('click', startButton);
-
- 
-
-
-
                                            
-
-
 
 
 //questa roba va inserita nella funzione e va sincronizzata con eleGame
@@ -71,4 +55,3 @@ eleStartButton.addEventListener('click', startButton);
 //     }
 
 // }
-
