1ceeb686a185cb769e3fd233c547b361a3c3f1cb
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-06 15:11:12 +0100

    univocità dei numeri

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const eleGame = document.querySelector('.game');
 const eleForm = document.querySelector('form');
 const eleInput = document.querySelector('input')
 
-const arrNumbers = ['1'];
+const arrNumbers = [];
 
 function startButton () {
     eleStartButton.classList.add('hidden');
@@ -30,39 +30,34 @@ function startButton () {
 
     setTimeout (function() {
         eleForm.classList.add('show');
-    }, 3740);
+    }, 37400);
 
-    const randomNumbers1 = Math.floor(Math.random() * 200);
-    const randomNumbers2 = Math.floor(Math.random() * 200);
-    const randomNumbers3 = Math.floor(Math.random() * 200);
-    const randomNumbers4 = Math.floor(Math.random() * 200);
-    const randomNumbers5 = Math.floor(Math.random() * 200);
+    const randomNumbers1 = (Math.floor(Math.random() * 200) + 1);
+    const randomNumbers2 = (Math.floor(Math.random() * 200) + 1);
+    const randomNumbers3 = (Math.floor(Math.random() * 200) + 1);
+    const randomNumbers4 = (Math.floor(Math.random() * 200) + 1);
+    const randomNumbers5 = (Math.floor(Math.random() * 200) + 1);
 
     for (let i = 0; i < 5; i++) {  
-        arrNumbers.push(randomNumbers1, randomNumbers2, randomNumbers3, randomNumbers4, randomNumbers5);       
-        // console.log(arrNumbers[i]);        
-       
+        if (!arrNumbers.includes(i)) {
+            arrNumbers.push(randomNumbers1, randomNumbers2, randomNumbers3, randomNumbers4, randomNumbers5); 
+            
+            // console.log(arrNumbers[i]);  
+        }
+              
         eleGame.innerHTML += arrNumbers[i] + ' ';
-
-        // if (userNumbers === arrNumbers[i]) {
-        // alert('Bravo');
-        // } else {
-        //     alert('nope');
-        // }
     }
 }
 
 eleStartButton.addEventListener('click', startButton);
 
-
 eleForm.addEventListener('submit', function (event) {
 	event.preventDefault();
 
 	for (let i = 0; i < arrNumbers.length; i++) {
-
+        //validazione del risultato
 		if (eleInput.value === arrNumbers[i]) {
 			console.log('trovata');
-            numbers = true;
 		} else {
             console.log('non trovata');
         }
